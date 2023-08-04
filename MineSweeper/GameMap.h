bc636db4622761144b3102252e8a6585ecb706cd
2790004z <2790004Z@student.gla.ac.uk>
2023-08-04 23:02:06 +0800

    Mine Sweeper

--- MineSweeper/GameMap.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Eason Zhang on 23/07/2023.
-//
-
-#include "GameLauncher.h"
-
-void initialGame(char mineBoard[DIFFICULT_BOARD_SIZE][DIFFICULT_BOARD_SIZE], char displayBoard[EASY_BOARD_SIZE][DIFFICULT_BOARD_SIZE], int boardSize);
-void displayGameBoard(char displayBoard[DIFFICULT_BOARD_SIZE][DIFFICULT_BOARD_SIZE], int mapSize);
--- MineSweeper/Menu.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Eason Zhang on 23/07/2023.
-//
-
-#include "GameLauncher.h"
-
-void mainMenu();
-void difficultyMenu();
-void optionsMenu();
